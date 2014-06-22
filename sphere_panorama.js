2b10d795614ae68e7dccb43f31f063d3df0b8cfd
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2014-06-23 00:26:11 +0400

    fixed jsdoc type for "onlyWebGL" parameter

--- sphere_panorama.js
+++ sphere_panorama.js
@@ -300,7 +300,7 @@ function ($, THREE, Modernizr) {
 		 * @prop {float} [fovMouseStep=2.0] Step of zoom by mouse wheel
 		 * @prop {boolean} [mouseWheelRequired=false] Module "jquery.mousewheel" is required
 		 * @prop {number} [fpsLimit=30] Limit frames per second of animation
-		 * @prop {number} [onlyWebGL=false] If WebGL is not supported - do not use canvas, make error
+		 * @prop {boolean} [onlyWebGL=false] If WebGL is not supported - do not use canvas, make error
 		 */
 		/**
 		 * @public
